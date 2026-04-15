1762a21783679772486f1a0871e00cdd9cb4acbe
Karlaleticiaavila <Kbliaass@gmail.com>
2026-04-14 18:04:39 -0700

    save initial UI sandbox

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import QuizScreen from "./pages/QuizScreen";
+
+function App() {
+  return <QuizScreen />;
+}
+
+
+  
+export default App
---
+++ frontend/src/pages/QuizScreen.tsx
@@ -0,0 +1,19 @@
+function QuizScreen() {
+  return (
+    <div>
+      <header>
+        <h1>QUIZ BRAIN</h1>
+
+        <div>
+          <h2>GAME</h2>
+          <h2>TEAM</h2>
+          <h2>RANKING</h2>
+        </div>
+
+        <button>JOIN GAME!</button>
+      </header>
+    </div>
+  );
+}
+
+export default QuizScreen;
