d8195ea281eb471f5a95157d1f4517175b259d6a
Hiroki Inoue <tyynh.hiroki@gmail.com>
2026-04-16 20:52:51 -0700

    Feat / added cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,11 +4,18 @@ dotenv.config();
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes";
 import quizRouter from "./routes/quiz.routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
 
 app.use("/users", userRouter);
 app.use("/quizzes", quizRouter);
